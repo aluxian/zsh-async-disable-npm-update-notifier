e5876728ca5c1206e9b91640b5dd7b4539051268
Alexandru Rosianu <me@aluxian.com>
2025-09-07 14:21:41 +0300

    disown

--- disable-npm-update-notifier.plugin.zsh
+++ disable-npm-update-notifier.plugin.zsh
@@ -2,4 +2,5 @@ function _npm_update_notifier_disable() {
   command -v npm >/dev/null && { grep -q 'update-notifier=false' ~/.npmrc || npm config set update-notifier false; }
 }
 
-_npm_update_notifier_disable &
+_npm_update_notifier_disable &!
+
